d3019b29e1f5f2e9a4f01eef43a326ee604e4d2d
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-03-19 11:47:30 +0900

    add-project-05

---
+++ students3.php
@@ -0,0 +1,8 @@
+<?php
+$students = array(
+    array("Beri Pahabol", 19, "beri.pahabol@gmail.com");
+    array("Marina Rumi", 17, "marina.rumi@gmail.com");
+    array("Yekson Bagau", 19, "yekson.bagau@gmail.com");
+    array("Stevania Wanggai", 19, "stevania.wanggai@gmail.com");
+;
+?>
